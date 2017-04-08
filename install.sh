044ef6e5e434a71da724939af235c93af5cf69b0
Muhammad Qureshi <mutahirqureshi@gmail.com>
2017-04-07 23:47:37 -0700

    pass full paths to create_link in install script

--- install.sh
+++ install.sh
@@ -4,23 +4,24 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "DIR=$DIR"
 
 function create_link {
-  source="${DIR}/$1"
-  target="${HOME}/${1/_/.}"
+  source="$1"
+  target="$2"
 
   printf "linking %s to %s..." $source $target
 
   if [ -e $target ]; then
-    printf "removing already existing %s..." $target
-    rm -rf $target
+    printf "moving already existing %s to %s.bak..." $target $target
+    mv "$target" "${target}.bak"
   fi
 
   ln -sf $source $target
   printf "DONE!\n"
 }
 
-for i in _*
+for i in "${DIR}"/_*
 do
-  create_link $i
+  base=$(basename "$i")
+  create_link "$i" "${HOME}/${base/_/.}"
 done
 
 # Install vim plugins using plug.vim
